10f6689e2073e81308c2fa29900249b30a9ed1c4
Alice Won <alicewon4@gmail.com>
2019-10-23 07:52:05 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -35,10 +35,10 @@ def square_array(numbers)
   newSquaredArray =[]
   i = 0
 
-  while i < numbers.length do
+    while i < numbers.length do
 
-    newSquaredArray.push(numbers[i]*numbers[i])
-    i += 1
-  end
-
+      newSquaredArray.push(numbers[i]*numbers[i])
+      i += 1
+    end
+  newSquaredArray
 end
