10a8780a9632805e1c71e0d7c954021c14d7282c
Alice Won <alicewon4@gmail.com>
2019-10-23 07:33:50 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,5 @@
+=begin
+
 def square_array(numbers)
   i = 0
   square_array = []
@@ -11,3 +13,19 @@ def square_array(numbers)
   end
 end
 return square_array([3,6,9])
+
+
+=end
+
+def square_array(numbers)
+  i = 0
+
+  while i < numbers.length do
+    puts numbers[i] 
+    square_array.push(numbers[i]*numbers[i])
+    i += 1
+  end
+    
+  end
+
+  
